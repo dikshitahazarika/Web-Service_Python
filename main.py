283dd15c358e6350cd0f23ea4f2908bb5c9eb26e
dikshitahazarika <33933386+dikshitahazarika@users.noreply.github.com>
2019-10-13 19:43:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ import re
 main = Flask(__name__)
 
 # Check if Flask has run successfully and service should return the following request.
+
 @main.route("/")
-def test():
-    html = "<h1><center>Pond Test</center></h1>\
+def descp():
+    html = "<h1><center>Pond5 Test</center></h1>\
         <P><center><em>GET /PING - REQUEST RETURNS “PONG”</em></center></P>\
         <P><center><em>GET /SYSTEM - REQUEST RETURNS A SYSTEM INFORMATION AND SERVICE VERSION.</em></center></P>\
         <P><center><em>GET /MEDIAINFO/ID - REQUEST RETURNS TITLE, FILENAME, SIZE, DIMENSIONS AND PRICE.</em></center></P>"
@@ -26,12 +27,13 @@ def ping(request):
 # platform import provided show all the information system & return JSON format.
 @main.route("/system")
 def system():
-    systeminfo = {"architecture": platform.architecture(),
+    systeminfo = {"platform": platform.architecture(),
                   "processor": platform.processor(),
                   # "S_version": "0.0.1",
                   "system": platform.system(),
                   "version": platform.version(),}
     return jsonify(systeminfo)
+
 # Get all media information using media_id it is the numbered code of the specific photo resource
 # eg: '12192732' is media id  from “ https://www.pond5.com/photo/12192732/night-city-skyline-and-moon.html ”.
 @main.route("/mediainfo/<media_id>", methods=["GET"])
@@ -64,10 +66,6 @@ def media(media_id):
        # "dim": dim,
     }
     return jsonify(imageinfo)
-
 if __name__ == "__main__":
       # http://127.0.0.1
     main.run(host="0.0.0.0", debug=True, port=8080)
-
-
-
